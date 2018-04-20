42a8ef6d580470074f9b939df4cb6883717ccf61
Stefan Bossbaly <sbossb@gmail.com>
2018-04-19 23:31:29 -0400

    Fixed bash errors

--- src/docker_entrypoint.sh
+++ src/docker_entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
 
-# Initialize CCache if it will be used
-if [ "$USE_CCACHE" = 1 ]; then
-	ccache -M $CCACHE_SIZE 2>&1
-fi
-
 # Initialize Git user information
 git config --global user.name $USER_NAME
 git config --global user.email $USER_MAIL
+git config --global color.ui false
 
 # Build
 cd "$SRC_DIR"
@@ -23,7 +19,7 @@ if [ "$SIGN_BUILDS" = true ]; then
     echo ">> [$(date)] SIGN_BUILDS = true but empty \$KEYS_DIR, generating new keys"
     for c in releasekey platform shared media; do
       echo ">> [$(date)]  Generating $c..."
-      "SRC_DIR/development/tools/make_key" "$KEYS_DIR/$c" "$KEYS_SUBJECT" <<< '' &> /dev/null
+      "$SRC_DIR/development/tools/make_key" "$KEYS_DIR/$c" "$KEYS_SUBJECT" <<< '' &> /dev/null
     done
   else
     for c in releasekey platform shared media; do
@@ -31,7 +27,7 @@ if [ "$SIGN_BUILDS" = true ]; then
         if [ ! -f "$KEYS_DIR/$c.$e" ]; then
           echo ">> [$(date)] SIGN_BUILDS = true and not empty \$KEYS_DIR, but \"\$KEYS_DIR/$c.$e\" is missing"
           exit 1
-        fi
+        fiec
       done
     done
   fi
@@ -41,13 +37,18 @@ if [ "$SIGN_BUILDS" = true ]; then
     openssl genrsa -out "$KEYS_DIR/avb.pem" 2048
   fi
   
-  if [ ! -f "$KEYS_DIR/avb_pkmd.bin" ]; the
-    "SRC_DIR/external/avb/avbtool" extract_public_key --key "$KEYS_DIR/avb.pem" --output "$KEYS_DIR/avb_pkmd.bin"
+  if [ ! -f "$KEYS_DIR/avb_pkmd.bin" ]; then
+    "$SRC_DIR/external/avb/avbtool" extract_public_key --key "$KEYS_DIR/avb.pem" --output "$KEYS_DIR/avb_pkmd.bin"
   fi
 fi
 
 # Sync work dir
 repo sync -j${NUM_OF_THREADS}
+
+# Initialize CCache if it will be used
+if [ "$USE_CCACHE" = 1 ]; then
+	"$SRC_DIR/prebuilts/misc/linux-x86/ccache/ccache" -M $CCACHE_SIZE 2>&1
+fi
 
 # Select device
 source script/copperhead.sh
@@ -60,12 +61,12 @@ rm -rf vendor/google_devices/${DEVICE}
 mv vendor/android-prepare-vendor/${DEVICE}/$(echo $BUILD_ID | tr '[:upper:]' '[:lower:]')/vendor/google_devices/${DEVICE} vendor/google_devices
 
 # The smaller variant of the pixels have to move their bigger brother's folder as well
-if ["$DEVICE" = "walleye" || "$DEVICE" = "sailfish"]; then
-  big_brother = ""
-  if ["$DEVICE" = "walleye"]; then
-    big_brother = "muskie"
+if [ "$DEVICE" = "walleye" ] || [ "$DEVICE" = "sailfish" ]; then
+  big_brother=""
+  if [ "$DEVICE" = "walleye" ]; then
+    big_brother="muskie"
   else
-    big_brother = "marlin"
+    big_brother="marlin"
   fi
   
   rm -fr vendor/google_devices/${big_brother}
@@ -91,6 +92,9 @@ if [ "$SIGNATURE_SPOOFING" = "yes" ] || [ "$SIGNATURE_SPOOFING" = "restricted" ]
   mkdir -p "vendor/$vendor/overlay/microg/frameworks/base/core/res/res/values/"
   cp /root/signature_spoofing_patches/frameworks_base_config.xml "vendor/$vendor/overlay/microg/frameworks/base/core/res/res/values/config.xml"
 fi
+
+# Link key directory
+ln -s "$KEYS_DIR" "$SRC_DIR/keys/${DEVICE}"
 
 # Build project
 make target-files-package -j${NUM_OF_THREADS}
